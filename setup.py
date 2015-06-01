c4fca928004d03f0365914ebc73dcbe0b08008eb
Andre Keller <andre.keller@vshn.ch>
2015-06-01 12:09:55 +0200

    New release 0.1.2.

--- setup.py
+++ setup.py
@@ -10,7 +10,7 @@ here = path.abspath(path.dirname(__file__))
 
 setup(
     name='tikapy',
-    version='0.1.1',
+    version='0.1.2',
     description='A python client for the MikroTik RouterOS API',
     url='https://github.com/vshn/tikapy',
     author='Andre Keller',
